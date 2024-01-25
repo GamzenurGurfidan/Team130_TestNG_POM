b4fe914015df163b9b722c5982e0bf63c8df356b
GamzenurGurfidan <gamzenurguclu@gmail.com>
2024-01-25 21:27:57 +0300

    day 14

--- src/test/java/tests/day15_testNG_POM/C02_Priority.java
+++ src/test/java/tests/day15_testNG_POM/C02_Priority.java
@@ -1,2 +1,23 @@
-package tests.day15_testNG_POM;public class C02_Priority {
+package tests.day15_testNG_POM;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C02_Priority {
+
+    // ● https://www.testotomasyonu.com/ adresine gidin.
+    // Olusturacaginiz 3 farkli test method’unda asagida verilen görevleri yapin.
+    // 1. Test : Testotomasyonu ana sayfaya gittiginizi test edin
+    // 2. Test : search Box’i kullanarak “phone” icin arama yapin
+    //           ve arama sonucunda urun bulunabildigini test edin
+    // 3.Test : ilk urunu tiklayin ve urun isminin case sensitive olmaksizin phone icerdigini test edin
+
+    @Test
+    public void testOtomasyonAnaSayfaTesti(){
+
+        Driver.getDriver().get("https://www.testotomasyonu.com");
+
+
+    }
+
 }
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,2 +1,29 @@
-package utilities;public class Driver {
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    /*
+        Bu classin amaci
+        belirlenen browsera uygun webDriver objesi olusturmak
+     */
+
+    public static WebDriver driver;
+
+    public static WebDriver getDriver(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        return driver;
+
+    }
+
 }
