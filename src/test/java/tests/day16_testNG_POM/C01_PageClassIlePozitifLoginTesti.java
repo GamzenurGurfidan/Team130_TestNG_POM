f174babd5bec75c971e881003730c2f9d5d115ea
GamzenurGurfidan <gamzenurguclu@gmail.com>
2024-01-25 23:24:58 +0300

    day 16

---
+++ src/test/java/tests/day16_testNG_POM/C01_PageClassIlePozitifLoginTesti.java
@@ -0,0 +1,30 @@
+package tests.day16_testNG_POM;
+
+import org.testng.annotations.Test;
+import pages.TestOtomasyonPage;
+import utilities.Driver;
+
+public class C01_PageClassIlePozitifLoginTesti {
+
+    @Test
+    public void pozitifLoginTesti(){
+
+        // 1- https://www.testotomasyonu.com/ anasayfasina gidin
+        // 2- account linkine basin
+        // 3- Kullanici email'i olarak “wise@gmail.com” girin
+        // 4- Kullanici sifresi olarak “12345” girin
+        // 5- Login butonuna basarak login olun
+        // 6- Basarili olarak giris yapilabildigini test edin
+
+        Driver.getDriver().get("https://www.testotomasyonu.com/");
+
+        //POMde page classlarinda locate ettigimiz WebElementler ulasmak icin
+        // page classlarindan obje olustururuz
+
+        TestOtomasyonPage testOtomasyonPage = new TestOtomasyonPage();
+        testOtomasyonPage.accountLinki.click();
+
+        testOtomasyonPage.emailKutusu.sendKeys("wise@gmail.com");
+
+    }
+}
