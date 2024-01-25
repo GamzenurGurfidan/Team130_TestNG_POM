090df3950b79acca2c735377f5431b9e4f6d0686
GamzenurGurfidan <gamzenurguclu@gmail.com>
2024-01-25 23:27:03 +0300

    driver

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -18,12 +18,28 @@ public class Driver {
     public static WebDriver getDriver(){
 
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        if (driver == null){
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
 
         return driver;
 
+    }
+    public static void closeDriver(){
+
+        if (driver != null){
+            driver.close();
+            driver = null;
+        }
+    }
+    public static void quitDriver(){
+
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
     }
 
 }
